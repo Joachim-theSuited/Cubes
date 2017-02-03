a3c448a4d5cb96206426dc59d0f9fb691a686e28
Joachim Schulze <joachim@thesuited.com>
2017-02-03 19:25:16 +0100

    included basic movement for player and camera in Sandbox scene

---
+++ Cubes 3D/Assets/Scripts/Camera/Strict3DFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Follow a given Transform while maintaining an offset. Does not affect rotation.
+/// </summary>
+public class Strict3DFollow : MonoBehaviour {
+
+    public Transform objectToFollow;
+
+    public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = objectToFollow.position + offset;
+	}
+}
---
+++ Cubes 3D/Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Basic controls for the player handles movement and rotation. Additional interaction with objects may be registered here but should be handled elsewhere.
+/// </summary>
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerControls : MonoBehaviour {
+    public float movementSpeed;
+
+    private Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get inputs
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal") * 10 / 3f, 0f, Input.GetAxis("Vertical"));
+        // set inputVector /= max(1, inputVector.magnitude) to disallow movement faster than 1 but allow smooth transitions
+        if (inputVector.magnitude > 0)
+        {
+            inputVector /= Mathf.Max(1, inputVector.magnitude);
+        }
+
+        // factor in set speed and deltaTime to get translation for this tick
+        Vector3 moveVector = inputVector * movementSpeed * Time.deltaTime;
+
+        // move it
+        _rigidbody.MovePosition(_rigidbody.position + moveVector);
+    }
+}
