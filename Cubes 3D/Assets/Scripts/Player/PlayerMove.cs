07f418e2c160851d191a17ab9295f608e2701f89
Rafael <leafar@thesuited.com>
2017-02-09 20:40:22 +0100

    added very basic test scene

---
+++ Cubes 3D/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+		
+    }
+	
+    // Update is called once per frame
+    void Update() {
+        GetComponent<CharacterController>().Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+    }
+}
