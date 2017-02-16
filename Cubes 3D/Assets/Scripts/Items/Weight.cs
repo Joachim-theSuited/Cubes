2e48990eb5c1072cc53276dfa27f3b06016c918b
Rafael <leafar@thesuited.com>
2017-02-16 21:14:11 +0100

    weights and pressure plate ported

---
+++ Cubes 3D/Assets/Scripts/Items/Weight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A weight that can be put onto a PressurePlate.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class Weight : ItemScript {
+
+    /// <summary>
+    /// Produces the same effect as dropping the weight.
+    /// </summary>
+    public override void Use(GameObject player) {
+        player.SendMessage(Messages.DROP_EQUIPPED);
+    }
+
+}
