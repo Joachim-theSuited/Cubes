750a85076c6db9481e23a25b6911da826a2347a9
Joachim Schulze <joachim@thesuited.com>
2017-04-27 20:32:05 +0200

    added escape as shortcut to end the game

---
+++ Cubes 3D/Assets/Scripts/System/MainMenuTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MainMenuTrigger : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Cancel") > 0) {
+			Application.Quit();
+		}
+	}
+}
