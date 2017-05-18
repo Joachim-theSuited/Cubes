145cd762dcff2a8afb854c357874051e40549639
Joachim Schulze <joachim@thesuited.com>
2017-05-18 20:37:42 +0200

    extended ProjectBuildScript to create output with timestamps

--- Cubes 3D/Assets/Editor/ProjectBuildScript.cs
+++ Cubes 3D/Assets/Editor/ProjectBuildScript.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEditor;
+using System;
+using System.IO;
 
 public class ProjectBuildScript
 {
@@ -9,18 +11,22 @@ public class ProjectBuildScript
     [MenuItem("Build Tools/Experimental Android Build")]
     static void BuildAndroid()
     {
-        BuildPipeline.BuildPlayer(scenes, "./Target/Android/Cubes3D.apk", BuildTarget.Android, BuildOptions.None);
+		BuildPipeline.BuildPlayer(scenes, GetBuildTarget("android", ".apk"), BuildTarget.Android, BuildOptions.None);
     }
 
     [MenuItem("Build Tools/Windows Desktop Build")]
     static void BuildWindows()
     {
-        BuildPipeline.BuildPlayer(scenes, "./Target/Win64/Cubes3D.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+		BuildPipeline.BuildPlayer(scenes, GetBuildTarget("win64", ".exe"), BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 
     [MenuItem("Build Tools/Linux Desktop Build")]
     static void BuildLinux()
     {
-        BuildPipeline.BuildPlayer(scenes, "./Target/Linux64/Cubes3D", BuildTarget.StandaloneLinux64, BuildOptions.None);
+		BuildPipeline.BuildPlayer(scenes, GetBuildTarget("linux64", ""), BuildTarget.StandaloneLinux64, BuildOptions.None);
     }
+
+	public static string GetBuildTarget(string platform, string fileExtension) {
+		return String.Format("./Target/Cubes3D_{0}_{1}/Cubes3D{2}", DateTime.Today.ToString("yyyy_MM_dd"), platform, fileExtension);
+	}
 }
