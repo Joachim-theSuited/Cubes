4668d9f114909ce2ab58f94cef7172a137678e9c
Rafael <leafar@thesuited.com>
2017-07-10 20:00:14 +0200

    player collider resized more meaningful input names

--- Cubes 3D/Assets/Scripts/Constants/Inputs.cs
+++ Cubes 3D/Assets/Scripts/Constants/Inputs.cs
@@ -4,18 +4,20 @@
 /// This enum contains all Inputs configured in the Input Manager for this project. They map to the string that can be used to access that specific input.
 /// Using the enum is advised to avoid magic strings in the code.
 /// </summary>
-public static class Inputs
-{
-	public static readonly string Horizontal = "Horizontal";
-	public static readonly string Vertical = "Vertical";
+public static class Inputs {
+    public static readonly string Horizontal = "Horizontal";
+    public static readonly string Vertical = "Vertical";
 
-	public static readonly string Fire1 = "Fire1";
-	public static readonly string Fire2 = "Fire2";
-	public static readonly string Fire3 = "Fire3";
+    public static readonly string Fire1 = "Fire1";
+    public static readonly string Fire2 = "Fire2";
+    public static readonly string Fire3 = "Fire3";
 
-	public static readonly string Cancel = "Cancel";
+    public static readonly string Cancel = "Cancel";
 
-	public static readonly string MouseX = "Mouse X";
-	public static readonly string MouseY = "Mouse Y";
+    public static readonly string MouseX = "Mouse X";
+    public static readonly string MouseY = "Mouse Y";
+
+    public static readonly string UseItem = "Fire1";
+    public static readonly string DropItem = "Fire2";
 }
 
