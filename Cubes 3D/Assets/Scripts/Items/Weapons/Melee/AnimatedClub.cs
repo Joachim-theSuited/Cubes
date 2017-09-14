d5e990be0458039b3d7b49bcacf15f161f395b44
Rafael <leafar@thesuited.com>
2017-09-14 20:57:14 +0200

    warning removed

--- Cubes 3D/Assets/Scripts/Items/Weapons/Melee/AnimatedClub.cs
+++ Cubes 3D/Assets/Scripts/Items/Weapons/Melee/AnimatedClub.cs
@@ -17,12 +17,12 @@ public class AnimatedClub : ItemScript {
     }
 
     void OnCollisionEnter(Collision coll) {
-		if(isSwinging && !hits.Contains(coll.rigidbody) && coll.collider.GetComponent<AbstractDamageReceiver>() != null) {
+        if(isSwinging && !hits.Contains(coll.rigidbody) && coll.collider.GetComponent<AbstractDamageReceiver>() != null) {
             try {
                 //check if the collider should be hit directly
                 //RequireReceiver, to cause an Exception
                 coll.collider.SendMessage(Messages.DAMAGE, damage, SendMessageOptions.RequireReceiver);
-            } catch(UnityException e) {
+            } catch(UnityException) {
                 //if no direct hit was triggered, send to main rigidbody
                 coll.rigidbody.SendMessage(Messages.DAMAGE, damage, SendMessageOptions.DontRequireReceiver);
             }
