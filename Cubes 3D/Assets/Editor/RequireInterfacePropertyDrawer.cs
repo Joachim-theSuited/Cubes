8c92c713574165b78361fe75aef887d542301a48
Rafael <leafar@thesuited.com>
2017-09-22 14:14:34 +0200

    scripts for displaying health and other bars

---
+++ Cubes 3D/Assets/Editor/RequireInterfacePropertyDrawer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Property drawer for the RequireInterface attribute. Ensures that a referenced MonoBehaviour implements a given interface.
+/// </summary>
+[CustomPropertyDrawer(typeof(RequireInterface))]
+public class RequireInterfacePropertyDrawer : PropertyDrawer {
+    
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        // the editor just lets you choose a GameObject and picks a MonoBehaviour
+        EditorGUI.ObjectField(position, property);
+        // if that has not the required type, try to find another Component that has it, else clear the field (if GetComponent returns null)
+        RequireInterface iface = attribute as RequireInterface;
+        if(property.objectReferenceValue != null && !iface.type.IsInstanceOfType(property.objectReferenceValue)) {
+            property.objectReferenceValue = (property.objectReferenceValue as MonoBehaviour).gameObject.GetComponent(iface.type);
+        }
+    }
+
+}
---
+++ Cubes 3D/Assets/Scripts/Utilities/RequireInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Use this attribute on a MonoBehaviour property to enforce that the referenced behaviour implements a certain interface.
+/// </summary>
+public class RequireInterface : PropertyAttribute {
+
+    public Type type{ get; private set; }
+
+    /// <param name="type">Type of the interface (or class) that is required.</param>
+    public RequireInterface(Type type) {
+        this.type = type;
+    }
+
+}
