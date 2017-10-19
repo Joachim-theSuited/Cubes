cb3524f1a7c0fe0412e48c8419d6af245b689d39
Joachim Schulze <joachim@thesuited.com>
2017-10-19 20:59:08 +0200

    intial design for the rotation HUD compass

---
+++ Cubes 3D/Assets/Scripts/GameUI/CompassRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates the HUD compass when the player rotates.
+/// </summary>
+[RequireComponent(typeof(RectTransform))]
+public class CompassRotation : MonoBehaviour {
+
+	public Transform player;
+	public float spriteWidth = 1024 * 0.75f;
+	public float zoomFactor;
+
+	private Vector3 initialPosition;
+	private Vector3 angularShift;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+		angularShift = new Vector3(spriteWidth * zoomFactor / 360f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = initialPosition - player.eulerAngles.y * angularShift;
+	}
+}
