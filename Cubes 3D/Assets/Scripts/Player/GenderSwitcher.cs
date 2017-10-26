ba39520167e62ce57f19db8a3921276262707323
Rafael <leafar@thesuited.com>
2017-10-26 20:44:12 +0200

    imported male model; added test script for switching between player models

---
+++ Cubes 3D/Assets/Scripts/Player/GenderSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenderSwitcher : MonoBehaviour {
+
+    public RuntimeAnimatorController animationController;
+
+    public GameObject male;
+    public GameObject female;
+
+    bool isMale = false;
+    GameObject last;
+
+    void Start() {
+        ResetModel();
+    }
+
+    void Update() {
+        if(Input.GetKeyUp(KeyCode.G)) {
+            isMale = !isMale;
+            ResetModel();
+        }
+    }
+
+    void ResetModel() {
+        Destroy(last);
+        if(isMale)
+            last = Instantiate(male, transform);
+        else
+            last = Instantiate(female, transform);
+
+        var anim = last.GetComponent<Animator>();
+        anim.runtimeAnimatorController = animationController;
+    }
+
+}
