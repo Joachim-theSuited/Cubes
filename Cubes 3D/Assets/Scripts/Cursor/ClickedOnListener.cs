f938328586250bcd06951dd0abeab8cd339cdc23
Joachim Schulze <joachim@thesuited.com>
2017-11-02 20:04:47 +0100

    first design for the character selection

---
+++ Cubes 3D/Assets/Scripts/Cursor/ClickedOnListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+[RequireComponent(typeof(Collider))]
+public class ClickedOnListener : MonoBehaviour {
+
+	private Collider _collider;
+
+	// Use this for initialization
+	void Start () {
+		_collider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown(Inputs.Fire1)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit) && hit.collider == _collider) {
+				// TODO: trigger event
+				print("detected a click on this collider");
+			}
+		}
+	}
+}
---
+++ Cubes 3D/Assets/Scripts/Cursor/PointAtCursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Point the transform of this object at the cursor registered by the main camera.
+/// </summary>
+public class PointAtCursor : MonoBehaviour {
+
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)) {
+			transform.LookAt(hit.point);
+		}
+	}
+}
