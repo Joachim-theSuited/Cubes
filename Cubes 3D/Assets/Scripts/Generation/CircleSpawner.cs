8493c79bb17f46541f6fc44b41def40c4f43d425
Rafael <leafar@thesuited.com>
2017-11-02 20:59:33 +0100

    more configuration options for torus spawning

--- Cubes 3D/Assets/Scripts/Generation/CircleSpawner.cs
+++ Cubes 3D/Assets/Scripts/Generation/CircleSpawner.cs
@@ -1,43 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Security.Cryptography;
 
 public class CircleSpawner : MonoBehaviour {
 
-	public float timeBetweenSpawns = 10f;
-	public Transform circleTemplate;
+    public float timeBetweenSpawns = 10f;
+    public float shrinkSpeed;
+    public float spawnScale;
+    public Transform circleTemplate;
 
-	private float timeSinceLastSpawn = 0f;
+    /// <summary>
+    /// Function for mapping a random value to a height, at which the object will be spawned
+    /// </summary>
+    public AnimationCurve heightCDF;
 
+    private float timeSinceLastSpawn = 0f;
 
-	void Start() {
-		timeSinceLastSpawn = timeBetweenSpawns;
-	}
+    void Start() {
+        timeSinceLastSpawn = timeBetweenSpawns;
+        StartCoroutine(Spawn());
+    }
 
-	// Update is called once per frame
-	void Update () {
-		timeSinceLastSpawn += Time.deltaTime;
+    IEnumerator Spawn() {
+        while(true) {
+            Transform circle = (Transform)Instantiate(circleTemplate, transform.position, Quaternion.LookRotation(transform.forward));
+            circle.localScale = new Vector3(spawnScale, 1f, spawnScale);
+            circle.Translate(0, heightCDF.Evaluate(Random.value), 0);
 
-		if(timeSinceLastSpawn >= timeBetweenSpawns) {
-			Transform circle = (Transform)Instantiate(circleTemplate, transform.position, Quaternion.LookRotation(transform.forward));
-			circle.localScale = new Vector3(1000f, 1f, 1000f);
-			timeSinceLastSpawn = 0f;
+            StartCoroutine(shrinkCircle(circle.gameObject, circle.localScale, Vector3.one, shrinkSpeed));
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+    }
 
-			StartCoroutine(shrinkCircle(circle.gameObject, circle.localScale, Vector3.one, 60f));
-		}
-	}
+    private IEnumerator shrinkCircle(GameObject thingToScale, Vector3 scaleFrom, Vector3 scaleTo, float scaleTime) {
+        float timePassed = 0f;
 
-	private IEnumerator shrinkCircle(GameObject thingToScale, Vector3 scaleFrom, Vector3 scaleTo, float scaleTime) {
-		float timePassed = 0f;
+        while(timePassed < scaleTime) {
+            thingToScale.transform.localScale = Vector3.Lerp(scaleFrom, scaleTo, timePassed / scaleTime);
 
-		while(timePassed < scaleTime) {
-			thingToScale.transform.localScale = Vector3.Lerp(scaleFrom, scaleTo, timePassed/scaleTime);
+            yield return new WaitForFixedUpdate();
+            timePassed += Time.deltaTime;
+        }
 
-			yield return new WaitForFixedUpdate();
-			timePassed += Time.deltaTime;
-		}
-
-		Destroy(thingToScale);
-	}
+        Destroy(thingToScale);
+    }
 }
