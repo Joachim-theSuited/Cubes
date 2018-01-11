5d07c67c3d5e4d9f4a5df179c5d0c840c774e25e
Rafael <leafar@thesuited.com>
2018-01-11 20:59:15 +0100

    scripts and prefabs for endless world (just water atm)

---
+++ Cubes 3D/Assets/Scripts/Procedural/ChunkedWorldInitialiser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Initialise the scene with a square of WorldChunks.
+/// Required size and tiling is determined from the chunk prefab.
+/// </summary>
+public class ChunkedWorldInitialiser : MonoBehaviour {
+
+    public WorldChunk chunk;
+
+    [TooltipAttribute("Add an extra border of this many chunks")]
+    public uint extraTiles;
+
+    // Use this for initialization
+    void Start() {
+        InitWorld();
+    }
+
+    public void InitWorld() {
+        int width = (int)(chunk.spawnDistance + extraTiles);
+        for(int x = -width; x <= width; ++x) {
+            for(int y = -width; y <= width; ++y) {
+                WorldChunk newChunk = chunk.SpawnAt(transform.position + x * Vector3.right * chunk.tileSize + y * Vector3.forward * chunk.tileSize);
+                if(newChunk != null)
+                    newChunk.transform.SetParent(transform);
+            }
+        }
+    }
+
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ChunkedWorldInitialiser))]
+public class ChunkedWorldInitialiserEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        ChunkedWorldInitialiser script = (ChunkedWorldInitialiser)target;
+        if(GUILayout.Button("Clear All Children")) {
+            while(script.transform.childCount > 0)
+                DestroyImmediate(script.transform.GetChild(0).gameObject);
+        }
+        if(GUILayout.Button("Test Init")) {
+            script.InitWorld();
+        }
+    }
+}
+#endif
---
+++ Cubes 3D/Assets/Scripts/Scripting/DestroyOnPlayerTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DestroyOnPlayerTrigger : MonoBehaviour {
+
+    public bool onEnter;
+    public bool onExit;
+
+    public GameObject objectToDestroy;
+
+    void OnTriggerEnter(Collider other) {
+        if(onEnter && other.tag == Tags.Player)
+            Destroy(objectToDestroy);
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(onExit && other.tag == Tags.Player)
+            Destroy(objectToDestroy);
+    }
+}
