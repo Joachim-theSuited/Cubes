d3d4cccaf95d63ea992cc17c6ba28e78b9f979bc
Rafael <leafar@thesuited.com>
2018-01-25 20:05:31 +0100

    don't always place island in chunk center
    also corrected position-based calculation of random seed

--- Cubes 3D/Assets/Scripts/Procedural/IslandPlacer.cs
+++ Cubes 3D/Assets/Scripts/Procedural/IslandPlacer.cs
@@ -13,30 +13,59 @@ public class IslandPlacer : MonoBehaviour, WorldChunkDecorator {
     [Range(0, 1)]
     public float probability;
 
+    [Range(0, 1)]
+    [Tooltip("How far from the chunk's center may the island be placed")]
+    public float spread;
+
     public int randomSeedOffset;
 
     void WorldChunkDecorator.Decorate(WorldChunk chunk) {
         Vector3 normedPosition = chunk.transform.position / chunk.tileSize;
 
-        int quadrant = 0; // a factor in [0,3]; not conventinal numbering
-        if(normedPosition.x < 0)
-            quadrant += 1;
-        if(normedPosition.z < 0)
-            quadrant += 2;
+        int x = (int)normedPosition.x;
+        int z = (int)normedPosition.z;
+        float angle = Mathf.Atan2(z, x);
+        if(angle < 0) // atan2 can return negative angles
+            angle += Mathf.PI * 2;
+        int quadrant = (int)(angle / (Mathf.PI / 2));
 
-        // remove quadrant information from coords
-        int x = (int)Mathf.Abs(normedPosition.x);
-        int z = (int)Mathf.Abs(normedPosition.z);
+        // remove quadrant information from coords by rotating into quadrant 0
+        switch(quadrant) {
+            case 0: // already set correctly
+                break;
+            case 1:
+                x = (int)normedPosition.z;
+                z = -(int)normedPosition.x;
+                break;
+            case 2:
+                x = -(int)normedPosition.x;
+                z = -(int)normedPosition.z;
+                break;
+            case 3:
+                x = -(int)normedPosition.z;
+                z = (int)normedPosition.x;
+                break;
+            default:
+                Debug.Log("Invalid quadrant: " + quadrant.ToString() + " !");
+                break;
+        }
+        Debug.Assert(x >= 0 && z >= 0, "Coordinates (" + x.ToString() + ", " + z.ToString() + ") do not lie in quadrant 0!");
+
         // map N^2 to N by diagonalisation
-        int flatIdx = (x * (x + 1) / 2 + z);
+        int diagonalRow = x + z - 1; // (0, 0) is a special case counts as ' row -1'
+        int flatIdx = (diagonalRow * (diagonalRow + 1) / 2 + z);
         // map different quadrants together into N
-        flatIdx = flatIdx * 4 + quadrant;
+        flatIdx = diagonalRow == -1 ? 0 : (flatIdx * 4 + quadrant + 1);
 
         // the randomisation is based on the position, to always get the same results on regeneration
         Random.InitState(flatIdx + randomSeedOffset);
 
-        if(Random.value < probability)
-            Instantiate(islandPrefab, chunk.transform.position, Quaternion.identity, chunk.transform);
+        if(Random.value < probability) {
+            Vector2 direction = Random.insideUnitCircle;
+            Vector3 offset = new Vector3(direction.x, 0, direction.y);
+            offset *= spread * chunk.tileSize / 2;
+            Instantiate(islandPrefab, chunk.transform.position + offset, Quaternion.identity, chunk.transform);
+        }
     }
 
 }
