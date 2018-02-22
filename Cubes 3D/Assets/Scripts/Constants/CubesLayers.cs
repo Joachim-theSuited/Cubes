463a5f7074deff728e96d4ee19f3e5acc5110f7c
Rafael <leafar@thesuited.com>
2018-02-22 20:42:36 +0100

    adapted sinking behaviour, again

--- Cubes 3D/Assets/Scripts/Constants/CubesLayers.cs
+++ Cubes 3D/Assets/Scripts/Constants/CubesLayers.cs
@@ -1,6 +1,13 @@
+using UnityEngine;
+
 public static class CubesLayers {
 
     public static readonly string Floors = "Floors";
 
+    public static readonly int FloorsMask = LayerMask.GetMask(CubesLayers.Floors);
+
+    public static readonly string Sinking = "Default without Water";
+
+    public static readonly int SinkingID = LayerMask.NameToLayer(Sinking);
 
 }
--- Cubes 3D/Assets/Scripts/Player/PlayerSinkBehaviour.cs
+++ Cubes 3D/Assets/Scripts/Player/PlayerSinkBehaviour.cs
@@ -6,30 +6,35 @@ using UnityEngine;
 /// When staying still on open water, the player should sink.
 /// This is achieved, by moving the player to a layer that does not collide with water.
 /// </summary>
-[RequireComponent(typeof(PlayerControls), typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerControls), typeof(Rigidbody), typeof(PlayerJumpBehaviour))]
 public class PlayerSinkBehaviour : MonoBehaviour {
-
+    
     public float sinkAfterSeconds;
 
-    [Layer]
-    public int sinkingLayer;
+    [Tooltip("Default and sinking drag")]
+    public Vector2 drags;
 
     private float startSinkingTime;
     private PlayerControls _controls;
+    private Rigidbody _rigidbody;
 
     void Start() {
         _controls = GetComponent<PlayerControls>();
+        _rigidbody = GetComponent<Rigidbody>();
         // allow the player some extra time at game start
         startSinkingTime = Time.time + 5 * sinkAfterSeconds;
     }
 
     void FixedUpdate() {
-        if(_controls.getLastMovement().sqrMagnitude != 0) {
+        // reset if there was movement or the player is on solid ground
+        if(_controls.getLastMovement().sqrMagnitude != 0 || Physics.CheckSphere(transform.position, .1f, CubesLayers.FloorsMask)) {
             startSinkingTime = Time.time + sinkAfterSeconds;
             gameObject.layer = 0; // Default
+            _rigidbody.drag = drags.x;
         }
         if(Time.time > startSinkingTime) {
-            gameObject.layer = sinkingLayer;
+            gameObject.layer = CubesLayers.SinkingID;
+            _rigidbody.drag = drags.y;
         }
     }
 }
