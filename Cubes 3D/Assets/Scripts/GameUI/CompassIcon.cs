9a866ede3bbf54b6076006fb60716bc9477deffc
Rafael <leafar@thesuited.com>
2018-03-14 17:41:18 +0100

    scripts for compass icons

---
+++ Cubes 3D/Assets/Scripts/GameUI/CompassIcon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script that sets the required properties for the CompassIcon material.
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class CompassIcon : MonoBehaviour {
+
+    GameObject _player;
+    Material _material;
+
+    void Start() {
+        _player = GameObject.FindWithTag(Tags.Player);
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    void Update() {
+        Vector3 worldDirection = _player.transform.InverseTransformPoint(transform.position);
+        worldDirection.y = 0; // ignore height differences
+
+        _material.SetFloat("playerDistance", worldDirection.magnitude);
+
+        float angle = Mathf.Atan2(worldDirection.z, worldDirection.x);
+        angle -= Mathf.PI / 2; // straight ahead should be zero
+        angle /= (2 * Mathf.PI); // normalise
+        // for the wrap-around we want the parameter to be in [-0.75, 0.25],
+        if(angle > 0.25)
+            angle -= 1;
+        if(angle < -0.75)
+            angle += 1;
+        
+        _material.SetFloat("screenOffset", angle);
+    }
+}
---
+++ Cubes 3D/Assets/Scripts/GameUI/CompassIconCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Disables cpu culling for the camera, as we position objects inside the shader.
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class CompassIconCamera : MonoBehaviour {
+
+    void Start() {
+        var cam = GetComponent<Camera>();
+        cam.cullingMatrix = Matrix4x4.zero;
+    }
+
+}
