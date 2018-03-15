1f0fc68e390a932212f800f954b816ccbe136346
Rafael <leafar@thesuited.com>
2018-03-15 21:11:00 +0100

    focus nearby icons on compass

--- Cubes 3D/Assets/Scripts/GameUI/CompassIcon.cs
+++ Cubes 3D/Assets/Scripts/GameUI/CompassIcon.cs
@@ -8,29 +8,51 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class CompassIcon : MonoBehaviour {
 
+    // TODO is this feature actually helpful?
+    [Tooltip("When the player is closer than this, always show the icon in center")]
+    public float focusDistance;
+
     GameObject _player;
     Material _material;
+
+    int playerDistanceID;
+    int screenOffsetID;
 
     void Start() {
         _player = GameObject.FindWithTag(Tags.Player);
         _material = GetComponent<SpriteRenderer>().material;
+
+        playerDistanceID = Shader.PropertyToID("playerDistance");
+        screenOffsetID = Shader.PropertyToID("screenOffset");
     }
 
     void Update() {
         Vector3 worldDirection = _player.transform.InverseTransformPoint(transform.position);
         worldDirection.y = 0; // ignore height differences
 
-        _material.SetFloat("playerDistance", worldDirection.magnitude);
+        float distance = worldDirection.magnitude;
+        _material.SetFloat(playerDistanceID, distance);
 
-        float angle = Mathf.Atan2(worldDirection.z, worldDirection.x);
-        angle -= Mathf.PI / 2; // straight ahead should be zero
-        angle /= (2 * Mathf.PI); // normalise
-        // for the wrap-around we want the parameter to be in [-0.75, 0.25],
-        if(angle > 0.25)
-            angle -= 1;
-        if(angle < -0.75)
-            angle += 1;
-        
-        _material.SetFloat("screenOffset", angle);
+        if(distance < focusDistance) {
+            _material.SetFloat(screenOffsetID, 0);
+        } else {
+            float angle = Mathf.Atan2(worldDirection.z, worldDirection.x);
+            angle -= Mathf.PI / 2; // straight ahead should be zero
+            angle /= 2 * Mathf.PI; // normalise
+            // for the wrap-around we want the parameter to be in [-0.75, 0.25],
+            if(angle > 0.25f)
+                angle -= 1;
+            if(angle < -0.75f)
+                angle += 1;
+
+            // gradually shift icon into focus, when the player closes in
+            if(distance < 2 * focusDistance) {
+                // the shift in the angle needs to be based on the [-.5, .5] range
+                float shiftAngle = angle > -0.5f ? angle : (angle + 1);
+                angle -= shiftAngle * (2 - distance / focusDistance);
+            }
+                        
+            _material.SetFloat(screenOffsetID, angle);
+        }
     }
 }
