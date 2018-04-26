110005ca436f75b1eafa70d0c5b5ee5962b004b5
Rafael <leafar@thesuited.com>
2018-04-26 20:39:50 +0200

    extended blender export helper

--- BlenderScripts/exportHelper.py
+++ BlenderScripts/exportHelper.py
@@ -6,6 +6,7 @@ bl_info = {
     }
 
 import bpy
+from bpy.props import *
 import mathutils
 
 from mathutils import Vector, Matrix
@@ -15,15 +16,28 @@ class ExportHelperOp(bpy.types.Operator):
     bl_label = "Prepare for Export"
     bl_options = {"REGISTER", "UNDO"}
 
+    bottom_offset = FloatProperty(
+        name = "Offset",
+        description = "Offset of the origin from the bottom of the mesh",
+        default = 0
+    )
+
+    scale_only = BoolProperty(
+        name = "Scale only",
+        description = "Don't move origin, only normalise scaling",
+        default = False
+    )
+
     def execute(self, context):
         for obj in context.selected_objects:
             if obj.type != 'MESH':
                 continue # just ignore for now
 
             # move origin to bottom
-            bb_center = sum( ( Vector(corner) for corner in obj.bound_box ) , Vector() ) / 8
-            obj.data.transform( Matrix.Translation( (0, 0, -bb_center.z + obj.dimensions.z/( 2 * obj.scale.z )) ) )
-            obj.location.z = 0
+            if not self.scale_only:
+                bb_center = sum( ( Vector(corner) for corner in obj.bound_box ) , Vector() ) / 8
+                obj.data.transform( Matrix.Translation( (0, 0, -bb_center.z + ( -self.bottom_offset + obj.dimensions.z/2 ) / obj.scale.z ) ) )
+                obj.location.z = 0
 
             # remove scaling
             scaleX = Matrix.Scale(obj.scale.x, 4, (1.0, 0.0, 0.0))
