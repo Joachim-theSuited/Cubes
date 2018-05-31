c4ef84c2344b6491b3c934c866eae012c2483ac3
Rafael <leafar@thesuited.com>
2018-05-31 21:07:09 +0200

    placement of different island types

--- Cubes 3D/Assets/Scripts/Procedural/IslandPlacer.cs
+++ Cubes 3D/Assets/Scripts/Procedural/IslandPlacer.cs
@@ -8,7 +8,9 @@ using UnityEngine;
 /// </summary>
 public class IslandPlacer : MonoBehaviour, WorldChunkDecorator {
 
-    public GameObject islandPrefab;
+    public int randomSeedOffset;
+
+    public GameObject[] islandPrefabs;
 
     [Range(0, 1)]
     public float probability;
@@ -17,7 +19,10 @@ public class IslandPlacer : MonoBehaviour, WorldChunkDecorator {
     [Tooltip("How far from the chunk's center may the island be placed")]
     public float spread;
 
-    public int randomSeedOffset;
+    [Tooltip("Square areas of size tiling will have the same island type")]
+    public int tiling;
+    [Tooltip("Below 1 for more perlin-like distribution. Above 1 for decorrelation of neighbouring tiles.")]
+    public float tileRandomness;
 
     void WorldChunkDecorator.Decorate(WorldChunk chunk) {
         Vector3 normedPosition = chunk.transform.position / chunk.tileSize;
@@ -52,7 +57,7 @@ public class IslandPlacer : MonoBehaviour, WorldChunkDecorator {
         Debug.Assert(x >= 0 && z >= 0, "Coordinates (" + x.ToString() + ", " + z.ToString() + ") do not lie in quadrant 0!");
 
         // map N^2 to N by diagonalisation
-        int diagonalRow = x + z - 1; // (0, 0) is a special case counts as ' row -1'
+        int diagonalRow = x + z - 1; // (0, 0) is a special case counts as 'row -1'
         int flatIdx = (diagonalRow * (diagonalRow + 1) / 2 + z);
         // map different quadrants together into N
         flatIdx = diagonalRow == -1 ? 0 : (flatIdx * 4 + quadrant + 1);
@@ -60,11 +65,28 @@ public class IslandPlacer : MonoBehaviour, WorldChunkDecorator {
         // the randomisation is based on the position, to always get the same results on regeneration
         Random.InitState(flatIdx + randomSeedOffset);
 
-        if(Random.value < probability) {
+        if(Random.value < probability && islandPrefabs.Length != 0) {
+            // randomise position in chunk
             Vector2 direction = Random.insideUnitCircle;
             Vector3 offset = new Vector3(direction.x, 0, direction.y);
             offset *= spread * chunk.tileSize / 2;
-            Instantiate(islandPrefab, chunk.transform.position + offset, Quaternion.identity, chunk.transform);
+
+            // choose island type
+            // we group squares of tiling*tiling WorldChunks
+            //Vector2 tileCoords = new Vector2((int)normedPosition.x / tiling, (int)normedPosition.z / tiling);
+            Vector2 tileCoords = new Vector2(Mathf.Round(normedPosition.x / tiling), Mathf.Round(normedPosition.z / tiling));
+            // Unity's perlin mirrors at 0, which we try to avoid with a huge offset
+            // also it seems to be uniformly .5 for integer coords, so we add some fractional fudge
+            Vector2 perlinCoords = tileCoords * tileRandomness + new Vector2(16000.375f, 16000.625f);
+            var noise = Mathf.PerlinNoise(perlinCoords.x, perlinCoords.y);
+            // rescale, as the interval ends seemed to be underrepresented
+            if(islandPrefabs.Length > 2) {
+                float rsFactor = 1f / islandPrefabs.Length;
+                noise = ( Mathf.Clamp(noise, rsFactor, 1-rsFactor) - rsFactor ) / ( 1 - 2*rsFactor );
+            }
+            int islandIdx = (int) Mathf.Clamp( noise * islandPrefabs.Length, 0, islandPrefabs.Length - 1);
+
+            Instantiate(islandPrefabs[islandIdx], chunk.transform.position + offset, Quaternion.identity, chunk.transform);
         }
     }
 
