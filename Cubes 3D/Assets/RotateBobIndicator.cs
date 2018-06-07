94c44f7d4f26d89e4f35f85ce0052781d35d1db8
Joachim Schulze <joachim@thesuited.com>
2018-06-07 21:58:39 +0200

    included interaction indicator in main menu and fixed a translation bug

--- Cubes 3D/Assets/RotateBobIndicator.cs
+++ Cubes 3D/Assets/RotateBobIndicator.cs
@@ -10,9 +10,16 @@ public class RotateBobIndicator : MonoBehaviour {
 	public Vector3 rotationPerFrame = new Vector3(0, 0, 1);
 	public Vector3 translationPerFrame = new Vector3(0, 0, 0.001f);
 	
+	private Vector3 startTranslation;
+
+	void Start() {
+		startTranslation = transform.localPosition;
+	}
+
+
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(rotationPerFrame);
-		transform.Translate(translationPerFrame * Mathf.Sin(Time.time));
+		transform.localPosition = startTranslation + translationPerFrame * Mathf.Sin(Time.time);
 	}
 }
