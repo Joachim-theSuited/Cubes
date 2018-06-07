ff2792b79f7636d06745428b127f7a83802cf864
Joachim Schulze <joachim@thesuited.com>
2018-06-07 21:06:05 +0200

    character selection functional

--- Cubes 3D/Assets/Scripts/Interaction/CallingInteractionTrigger.cs
+++ Cubes 3D/Assets/Scripts/Interaction/CallingInteractionTrigger.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
 
+/// <summary>
+/// A variant of the InteractionTrigger that does direct calls instead of using messages.
+/// </summary>
 [RequireComponent(typeof(Collider))]
 public abstract class CallingInteractionTrigger : InteractionTrigger {
 
