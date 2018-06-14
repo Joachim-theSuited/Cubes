c89a4b889e6065f647fc458314b49b5edb515b75
Joachim Schulze <joachim@thesuited.com>
2018-06-14 20:40:07 +0200

    implemented a simple game exit
    still needs a label

---
+++ Cubes 3D/Assets/ExitGameTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ExitGameTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag.Equals(Tags.Player)) {
+			Debug.Log("Application.Quit() called; not effective in editor");
+			Application.Quit();
+		}
+	}
+}
