c75bd6ceda665a75e9ea62c31f7a504c8dbd6549
Joachim Schulze <joachim@thesuited.com>
2018-09-06 20:47:10 +0200

    sconces now have an ambient crackling sound

---
+++ Cubes 3D/Assets/Scripts/Audio/RandomClipStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This small script will start the playback of an AudioSource, that is also attached to this GameObject, at a random point.
+/// 
+/// Useful for ambient sounds, so they are not in sync and sound artifical.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class RandomClipStart : MonoBehaviour {
+
+	void Start () {
+		AudioSource audioSource = GetComponent<AudioSource>();
+		audioSource.time = Random.Range(0, audioSource.clip.length);
+	}
+}
