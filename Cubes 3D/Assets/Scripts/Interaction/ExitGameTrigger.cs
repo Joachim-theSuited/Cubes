090f0d70511d54af537ac96db77d23f45687c2aa
Joachim Schulze <joachim@thesuited.com>
2018-10-30 14:26:19 +0100

    confirmation dialog for the exit portal

--- Cubes 3D/Assets/Scripts/Interaction/ExitGameTrigger.cs
+++ Cubes 3D/Assets/Scripts/Interaction/ExitGameTrigger.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Collider))]
 public class ExitGameTrigger : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if(other.tag.Equals(Tags.Player)) {
-			Debug.Log("Application.Quit() called; not effective in editor");
-			Application.Quit();
+			GameFlow.PauseGame();
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+			GetComponentInChildren<Canvas>().enabled = true;
+			foreach(Button child in GetComponentsInChildren<Button>()) {
+				child.enabled = true;
+			}
+		}
+	}
+
+	public void ResumeGame() {
+		GameFlow.ResumeGame();
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		GetComponentInChildren<Canvas>().enabled = false;
+		foreach(Button child in GetComponentsInChildren<Button>()) {
+			child.enabled = false;
 		}
 	}
 }
