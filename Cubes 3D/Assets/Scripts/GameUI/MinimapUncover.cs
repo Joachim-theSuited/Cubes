ebf3bfa1b64f0a97d1ab9d5c6a647e09e519830c
Rafael <leafar@thesuited.com>
2019-02-21 20:36:06 +0100

    the minimap is now uncovered through exploration or picking up the map

---
+++ Cubes 3D/Assets/Scripts/GameUI/MinimapUncover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class MinimapUncover : MonoBehaviour
+{
+	public float fadeDuration;
+
+	int materialColorId;
+
+	void Start()
+	{
+		materialColorId = Shader.PropertyToID("_Color");
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == Tags.Player)
+			Uncover();
+    }
+
+	void Uncover()
+	{
+		StartCoroutine(FadeColor());
+        Destroy(gameObject, fadeDuration);
+	}
+
+	IEnumerator FadeColor()
+	{
+		while(true)
+		{
+			var material = GetComponent<MeshRenderer>().material;
+			var color = material.GetColor(materialColorId);
+			color.a -= Time.deltaTime / fadeDuration;
+			material.SetColor(materialColorId, color);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
---
+++ Cubes 3D/Assets/Scripts/Interaction/DungeonMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonMap : MonoBehaviour {
+
+	public GameObject minimapCover;
+
+	void Interact()
+	{
+		minimapCover.BroadcastMessage("Uncover", SendMessageOptions.DontRequireReceiver);
+		Destroy(this.gameObject);	
+	}
+	
+}
