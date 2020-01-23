cecaa981beaa6c84051bbe6316fead665c619b0b
Emily <joachim@thesuited.com>
2020-01-23 20:32:55 +0100

    increased jump strength to be able to jump with higher physics ticks
    also small changes to when impact sound and particles are spawned

--- Cubes 3D/Assets/Scripts/Player/PlayerJumpBehaviour.cs
+++ Cubes 3D/Assets/Scripts/Player/PlayerJumpBehaviour.cs
@@ -5,16 +5,17 @@
 /// Allows controlled player to jump to a certain height based on global gravity; the higher the gravity, the higher you can jump
 /// </summary>
 [RequireComponent(typeof(PlayerControls))]
-public class PlayerJumpBehaviour : MonoBehaviour {
+public class PlayerJumpBehaviour : MonoBehaviour
+{
 
     private Rigidbody _rigidbody;
     private ParticleSystem _jumpParticles;
     private bool canJump = true;
     private float chargeTime = 0f;
 
-    public float jumpForce = 200f;
+    public float jumpForce = 500f;
 
-    public readonly string[] jumpResetLayers = {CubesLayers.Floors, CubesLayers.Water};
+    public readonly string[] jumpResetLayers = { CubesLayers.Floors, CubesLayers.Water };
 
     public AudioSource jumpSound;
     public AudioSource impactSound;
@@ -23,18 +24,22 @@ public class PlayerJumpBehaviour : MonoBehaviour {
     private float lastFrameAltitudeChange;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         _rigidbody = GetComponent<Rigidbody>();
         _jumpParticles = GetComponent<ParticleSystem>();
     }
-	
+
     // Update is called once per frame
-    void Update() {
-        if(canJump && Input.GetButton(Inputs.Jump)) {
+    void Update()
+    {
+        if (canJump && Input.GetButton(Inputs.Jump))
+        {
             chargeTime += Time.deltaTime;
         }
 
-        if(canJump && Input.GetButtonUp(Inputs.Jump)) {
+        if (canJump && Input.GetButtonUp(Inputs.Jump))
+        {
             jumpSound.Play();
             _rigidbody.AddForce(Vector3.up * jumpForce * (1 + Mathf.Min(chargeTime, 2f)));
             canJump = false;
@@ -43,26 +48,37 @@ public class PlayerJumpBehaviour : MonoBehaviour {
             chargeTime = 0;
         }
 
-        if(lastFrameAltitudeChange > 0 && _rigidbody.velocity.y < 0) {
+        if (lastFrameAltitudeChange > 0 && _rigidbody.velocity.y < 0)
+        {
             dropStart = _rigidbody.position.y;
         }
         lastFrameAltitudeChange = _rigidbody.velocity.y;
     }
 
-    void OnCollisionEnter(Collision collision) {
+    void OnCollisionEnter(Collision collision)
+    {
         // when landing on a floor reenable jumping
         int layerMask = 1 << collision.gameObject.layer;
-        if((layerMask & LayerMask.GetMask(jumpResetLayers)) == layerMask && dropStart - _rigidbody.position.y > 0.5f) {
+        if ((layerMask & LayerMask.GetMask(jumpResetLayers)) == layerMask)
+        {
             canJump = true;
             // can be moved into animation once we have a 'falling' animation state
-            _jumpParticles.Emit(50);
         }
 
-        // play impact sound only when dropped a certain height
-        ImpactSoundDispatch impactSoundDispatch = collision.transform.GetComponent<ImpactSoundDispatch>();
-        if(impactSoundDispatch != null && dropStart - _rigidbody.position.y > 0.5f) {
-            impactSoundDispatch.playRandomlyDrawn();
-            dropStart -= 1000;
+
+        float distanceFallen = dropStart - _rigidbody.position.y;
+        if (distanceFallen > 0.5f)
+        {
+            _jumpParticles.Emit(50);
+
+            // play impact sound only when dropped a certain height
+            ImpactSoundDispatch impactSoundDispatch = collision.transform.GetComponent<ImpactSoundDispatch>();
+            if (impactSoundDispatch != null && distanceFallen > 0.5f)
+            {
+                impactSoundDispatch.playRandomlyDrawn();
+                dropStart -= 1000;
+            }
         }
+
     }
 }
