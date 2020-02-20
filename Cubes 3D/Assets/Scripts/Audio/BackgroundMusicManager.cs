4d786b70dce4448b762310fa4d962779887b6e61
Emily <joachim@thesuited.com>
2020-02-20 20:54:50 +0100

    background music is not restarted when switching scenes

---
+++ Cubes 3D/Assets/Scripts/Audio/BackgroundMusicManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusicManager : MonoBehaviour
+{
+
+    public static AudioClip LAST_CLIP = null;
+    public static float LAST_POSITION = 0f;
+
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+
+        if (audioSource.clip == LAST_CLIP)
+        {
+            audioSource.time = LAST_POSITION;
+            print("seeking to " + audioSource.time);
+        }
+    }
+
+    void Update() {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        LAST_CLIP = audioSource.clip;
+        LAST_POSITION = GetComponent<AudioSource>().time;
+    }
+}
