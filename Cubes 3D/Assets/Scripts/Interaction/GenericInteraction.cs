ed57e249c95a535306879dcb51ec712544a761a4
Rafael <leafar@thesuited.com>
2020-10-15 20:49:39 +0200

    Add UnityEvent-based interaction script

---
+++ Cubes 3D/Assets/Scripts/Interaction/GenericInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericInteraction : MonoBehaviour {
+
+    public UnityEvent onInteractEvents;
+
+	void Interact(){
+        onInteractEvents.Invoke();
+    }
+
+}
